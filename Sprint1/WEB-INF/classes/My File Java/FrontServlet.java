3327ff1893cf9260ef2d31ff86a33a8af8fa7fa3
judi1968 <judiherinirina04@gmail.com>
2023-03-10 21:24:36 +0000

    sprint1

---
+++ Sprint1/WEB-INF/classes/My File Java/FrontServlet.java
@@ -0,0 +1,25 @@
+ package etu1968.framework.servlet;
+ import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+
+/**
+ * FrontServlet
+ */
+public class FrontServlet extends HttpServlet{
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        processRequest(req,res);
+    }
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        processRequest(req,res);
+    }
+    public void processRequest(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        String requestUrl = req.getRequestURL().toString();
+        String host = req.getHeader("Host");
+        PrintWriter out = res.getWriter();
+        requestUrl = requestUrl.split("//")[1].replace(host, "");
+        out.print(requestUrl);
+    }
+
+}
