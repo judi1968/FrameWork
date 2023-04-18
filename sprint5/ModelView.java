9c934c379980a57d5aaf39b6fbba8f2a623098b2
judi1968 <judiherinirina04@gmail.com>
2023-04-18 15:16:06 +0100

    sprint5

---
+++ sprint5/ModelView.java
@@ -0,0 +1,17 @@
+/**
+ * ModelView
+ */
+package etu1968.framework.servlet;
+/**
+ *
+ * @author judi
+ */
+public class ModelView {
+    String url;
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    public String getUrl() {
+        return url;
+    }
+}
